9ae7f4854a1abec6a6de06a4205926626a4f5f8a
Ilker Parmaksiz <ilkerparmaksiz@windowslive.com>
2026-03-02 14:42:02 -0600

    Transfering

---
+++ laropticks/PDFullSimOpticks_Module.cc
@@ -0,0 +1,251 @@
+/**
+ *  Class: PDFullSimOpticks
+ *  Plugin Type: producer
+ *  File: laropticks/PDFullSimOpticks_Module.cc
+ *  Author: Ilker Parmaksiz
+ *  Experiment: DUNE
+ *  Institution: Rice University
+ *  Date: 3/2/2026
+ *  Description: Module interface LArSoft with Opticks. Using IonAndScint Module, optical photon information is passed to opticks library.
+ *  The full photon propagation is simulated at GPU.
+ *  Input: 'sim::SimEnergyDeposit' Output: 'sim::OpDetBacktrackerRecord'
+**/
+
+// LArSoft libraries
+#include "larcore/CoreUtils/ServiceUtil.h"
+#include "larcore/Geometry/Geometry.h"
+#include "larcorealg/Geometry/BoxBoundedGeo.h"
+#include "larcorealg/Geometry/OpDetGeo.h"
+#include "larcoreobj/SimpleTypesAndConstants/geo_vectors.h"
+#include "lardataobj/Simulation/OpDetBacktrackerRecord.h"
+#include "lardataobj/Simulation/SimEnergyDeposit.h"
+#include "lardataobj/Simulation/SimPhotons.h"
+#include "larsim/IonizationScintillation/ISTPC.h"
+#include "nurandom/RandomUtils/NuRandomService.h"
+
+// Art libraries
+#include "art/Framework/Core/EDProducer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Utilities/make_tool.h"
+#include "canvas/Utilities/Exception.h"
+#include "canvas/Utilities/InputTag.h"
+#include "cetlib_except/exception.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "fhiclcpp/types/Atom.h"
+#include "fhiclcpp/types/Comment.h"
+#include "fhiclcpp/types/DelegatedParameter.h"
+#include "fhiclcpp/types/Name.h"
+#include "fhiclcpp/types/OptionalDelegatedParameter.h"
+#include "fhiclcpp/types/Sequence.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+// Random numbers
+#include "CLHEP/Random/RandPoissonQ.h"
+
+#include "range/v3/view/enumerate.hpp"
+
+#include <cmath>
+#include <ctime>
+#include <map>
+#include <memory>
+#include <vector>
+
+// laropticks
+#include "laropticks/include/OpticksInterface.h"
+
+// Geant4
+
+
+namespace laropticks {
+  class PDFullSimOpticks : public art::EDProducer {
+  public:
+    // Define the fhicl configuration
+    struct Config {
+        using Name = fhicl::Name;
+        using Comment = fhicl::Comment;
+        using DP = fhicl::DelegatedParameter;
+        using ODP = fhicl::OptionalDelegatedParameter;
+        const std::vector<int> default_TPCs{};
+        fhicl::Atom<art::InputTag> SimulationLabel{Name("SimulationLabel"),
+                                                   Comment("SimEnergyDeposit label.")};
+    };
+
+
+    using Parameters = art::EDProducer::Table<Config>;
+
+    explicit PDFullSimOpticks(Parameters const& config);
+    void produce(art::Event&) override;
+    void beginJob() override;
+    void endJob() override;
+
+  private:
+
+    std::vector<geo::Point_t> opDetCenters() const;
+    // geometry properties
+    geo::GeometryCore const& fGeom;
+    const size_t fNOpChannels;
+    const std::vector<geo::Point_t> fOpDetCenter;
+    OpticksInterface* opticks;
+  };
+
+  //--------------------Construct PDFullSimOpticks-----------------------------//
+  /*!
+   * Construct with fhicl parameters if there is any.
+   */
+  PDFullSimOpticks(Parameters const& config) : art::EDProducer{config}
+                                             , fNOpChannels(fGeom.NOpDets())
+                                             , fOpDetCenter(opDetCenters())
+{
+
+     mf::LogInfo("PDFullSimOpticks") << "Initializing PDFullSimOpticks." << std::endl;
+
+
+    // Initialize OpDetBacktrackerRecord
+    produces<std::vector<sim::OpDetBacktrackerRecord>>();
+
+    // Initialize Opticks
+    opticks=OpticksInterface::GetInstance();
+  }
+
+
+  //......................................................................
+  void PDFastSimPAR::produce(art::Event& event)
+  {
+     mf::LogTrace("PDFullSimOpticks") << "PDFullSimOpticks Module Producer"
+                              << "EventID: " << event.event();
+
+    art::Handle<std::vector<sim::SimEnergyDeposit>> edepHandle;
+    if (!event.getByLabel(fSimTag, edepHandle)) {
+      mf::LogError("PDFullSimOpticks") << "PDFullSimOpticks Module Cannot getByLabel: " << fSimTag;
+      return;
+    }
+
+    auto mcHandle = event.getValidHandle<std::vector<simb::MCParticle>>("largeant");
+    fOpticalPropagationTool->SetParticleList(  mcHandle);
+
+    auto const& edeps = edepHandle;
+    // Include energy deposits here
+
+    if(!World) init();
+	// init tracking
+	if(Trackmps==nullptr) initTracks();
+	std::cout << "OpticksInterface::executeEvent" << std::endl;
+
+    mf::LogTrace("OpticksInterface::executeEvent") << "Using Opticks tool";
+	// Optical Back Tracker
+  	//fOpDetBacktrackerMap = nullptr;
+
+	auto records=std::make_unique<std::vector<sim::OpDetBacktrackerRecord>>();
+
+    int num_points = 0;
+    int num_fastph = 0;
+    int num_slowph = 0;
+    int num_fastdp = 0;
+    int num_slowdp = 0;
+
+
+    mf::LogTrace("OpticksInterface::executeEvent")<< "Edep size " << edeps.size() << "\n";
+
+    int nphot;
+  	double edeposit;
+
+	std::cout << "Amount of Particles " << fParticleList->size() << std::endl;
+	// Get The Parent Information
+	std::cout << " Size of Energy Depositions " <<  edeps.size() << std::endl;
+	G4Track * aTrack=nullptr;
+
+	int tempTrackID=edeps.at(0).TrackID();
+	auto it = Trackmps->find(tempTrackID);
+	if (it != Trackmps->end()) aTrack = it->second;
+	else {
+		std::cout<<"No Track Found"<<std::endl;
+		assert(false);
+	}
+    for (auto const& edepi : edeps) {
+        if (!(num_points % 1000))
+		{
+		  std::cout <<" Opticks "
+          //mf::LogTrace("OpticksInterface")
+            << "SimEnergyDeposit: " << num_points << " " << edepi.TrackID() << " " << edepi.Energy()
+            << "\nStart: " << edepi.Start() << "\nEnd: " << edepi.End()
+            << "\nNF: " << edepi.NumFPhotons() << "\nNS: " << edepi.NumSPhotons()
+            << "\nSYR: " << edepi.ScintYieldRatio()
+			<< "PDG: " << edepi.PdgCode()<<"\n"
+            << std::endl;
+
+        }
+		if (tempTrackID != edepi.TrackID()){
+			std::cout << "TempTrack_ID " << tempTrackID << " Track_ID " <<edepi.TrackID() << std::endl;
+			tempTrackID = edepi.TrackID();
+			it = Trackmps->find(tempTrackID);
+			if (it != Trackmps->end()) aTrack = it->second;
+			else {
+				std::cout<<"No Track Found"<<std::endl;
+				assert(false);
+			}
+		}
+		CollectPhotons(aTrack,edepi);
+
+        num_points++;
+
+		nphot=edepi.NumPhotons();
+
+        edeposit = edeposit + edepi.Energy();
+		nphot+=edepi.NumSPhotons();
+        num_fastph +=edepi.NumFPhotons();
+        num_slowph +=edepi.NumSPhotons();
+
+	    mf::LogTrace("OpticksInterface:executeEvent")
+	    << "Total points: " << num_points << ", total fast photons: " << num_fastph
+	    << ", total slow photons: " << num_slowph << "\ndetected fast photons: " << num_fastdp
+	    << ", detected slow photons: " << num_slowdp;
+	}
+
+	Simulate();
+	if(obtrHelpers.size()>0){
+		for (auto& opbtr: obtrHelpers)
+			records->emplace_back(opbtr.second);
+		event.put(std::move(records));
+	} else std::cout << "obtrHelper seems empty ...." << std::endl;
+
+
+	Trackmps->clear();
+	delete Trackmps;
+	Trackmps=nullptr;
+	eventID++;
+  }
+
+
+  //......................................................................
+  std::vector<geo::Point_t> PDFastSimPAR::opDetCenters() const
+  {
+    std::vector<geo::Point_t> opDetCenter;
+    for (size_t const i : ::ranges::views::ints(size_t(0), fNOpChannels)) {
+      geo::OpDetGeo const& opDet = fGeom.OpDetGeoFromOpDet(i);
+      opDetCenter.push_back(opDet.GetCenter());
+    }
+    return opDetCenter;
+  }
+  // ---------------------------------------------------------------------------
+
+} // namespace laropticks
+
+//--------------------Begin Job----------------------------------------------//
+void phot::OpticalPropagation::beginJob()
+{
+  mf::LogTrace("PDFullSimOpticks") << "beginJob" << std::endl;
+  opticks->beginJob();
+}
+
+//--------------------End Job---------------------------------------------//
+void phot::OpticalPropagation::endJob()
+{
+  mf::LogTrace("PDFullSimOpticks") << "endJob" << std::endl;
+  opticks->endJob();
+}
+
+
+DEFINE_ART_MODULE(laropticks::PDFullSimOpticks)
